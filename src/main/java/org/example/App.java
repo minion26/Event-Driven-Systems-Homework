1e2efbd803df536398c1bed82f362d7fa9e6e8b8
Jitca Diana <jitcadiana6@gmail.com>
2025-03-31 18:50:27 +0300

    created a topology and bolt only for logging information

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,71 @@
 package org.example;
 
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.generated.StormTopology;
+import org.apache.storm.topology.TopologyBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final String SPOUT_ID_PUBLISHER = "publisher_spout";
+    private static final String SPOUT_ID_SUBSCRIPTION = "subscription_spout";
+    private static final String BOLT_ID_BASIC = "basic_bolt";
+
+
+    public static void main( String[] args ) throws Exception {
+
+        // precizam frecventa unor campuri
+        Map<String, Double> fieldFrequencies = new HashMap<>();
+        fieldFrequencies.put("city", 0.9);
+        fieldFrequencies.put("temp", 0.8);
+        fieldFrequencies.put("wind", 0.7);
+
+        // precizam frecventa pentru egalitate
+        Map<String, Double> equalityFrequencies = new HashMap<>();
+        equalityFrequencies.put("city", 0.7);
+
+
+        // construim topologia
+        TopologyBuilder builder = new TopologyBuilder();
+        SubscriptionSpout subscriptionSpout = new SubscriptionSpout(fieldFrequencies, equalityFrequencies);
+        PublisherSpout publisherSpout = new PublisherSpout();
+        BasicBolt basicBolt = new BasicBolt();
+
+
+        builder.setSpout(SPOUT_ID_PUBLISHER, subscriptionSpout);
+        builder.setSpout(SPOUT_ID_SUBSCRIPTION, publisherSpout);
+        builder.setBolt(BOLT_ID_BASIC, basicBolt).shuffleGrouping(SPOUT_ID_SUBSCRIPTION).shuffleGrouping(SPOUT_ID_PUBLISHER);
+
+
+        Config config = new Config();
+
+        LocalCluster cluster = new LocalCluster();
+        StormTopology topology = builder.createTopology();
+
+        // fine tuning
+        config.put(Config.TOPOLOGY_EXECUTOR_RECEIVE_BUFFER_SIZE,1024);
+        config.put(Config.TOPOLOGY_TRANSFER_BATCH_SIZE,1);
+
+
+        cluster.submitTopology("first_topology", config, topology);
+
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // opreste executia unei topologii
+        cluster.killTopology("first_topology");
+        cluster.shutdown();
+
+        cluster.close();
     }
 }
---
+++ src/main/java/org/example/BasicBolt.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.Map;
+
+public class BasicBolt extends BaseRichBolt {
+
+    private OutputCollector collector;
+
+    @Override
+    public void prepare(Map<String, Object> map, TopologyContext topologyContext, OutputCollector outputCollector) {
+        this.collector = outputCollector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+
+        System.out.println("Received: " + tuple.getValue(0));
+
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+}
