a938118ffdeb4929e5ff5f37a4b5ab91845d26cf
Jitca Diana <jitcadiana6@gmail.com>
2025-04-07 14:23:57 +0300

    adaugat scrierea tuplelor in fisiere

--- src/main/java/org/example/BasicBolt.java
+++ src/main/java/org/example/BasicBolt.java
@@ -1,11 +1,35 @@
 package org.example;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class BasicBolt  {
 
+    private final BufferedWriter writer;
+
+    public BasicBolt() throws IOException {
+        // Generate a unique file name using a timestamp
+        String timestamp = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").format(LocalDateTime.now());
+        this.writer = new BufferedWriter(new FileWriter("tuples_" + timestamp + ".txt"));
+    }
 
     public void execute(Object tuple) {
         System.out.println("Received: " + tuple);
+        try {
+            writer.write(tuple.toString());
+            writer.newLine();
+            writer.flush(); // Ensure data is written to the file
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    public void close() throws IOException {
+        writer.close();
     }
 
 
